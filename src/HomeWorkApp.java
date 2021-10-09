9ef17a48094785f71649d3151adaf5c73ef3702f
DanyRDS <dborodin3@gmail.com>
2021-10-09 22:42:59 +0300

    Второе домашнее задание

--- src/HomeWorkApp.java
+++ src/HomeWorkApp.java
@@ -12,6 +12,34 @@ public class HomeWorkApp {
         System.out.println("");
         System.out.println("Задание 5");
         compareNumbers();
+
+        //Домашнее задание 2
+        //объявим переменные
+        int x = 2;
+        int y = 13;
+        int year = 2004;
+        String message = "привет";
+
+        // Запустим первый метод
+        // Вернем значение true или false
+        boolean t = addTwoNumbers(x, y);
+        System.out.println(t);
+
+        // Проверим второй метод
+        number(x);
+
+        // Запустим третий метод
+        // Вернем значение true или false
+        boolean u = numberBoolean(x);
+        System.out.println(u);
+
+        // Проверим четвертый метод
+        cycle(x, message);
+
+        // Пятый метод (високосный год)
+        Boolean p = years(year);
+        System.out.println(p);
+
     }
     public static void printThreeWords() {
         System.out.println("orange");
@@ -50,6 +78,57 @@ public class HomeWorkApp {
             System.out.println("a >= b");
         } else if (a < b) {
             System.out.println("a < b");
+        }
+    }
+// Домашнее задание 2
+    // Задание 1
+
+    public static boolean addTwoNumbers(int a, int b) {
+        int c = a + b;
+        if (c >= 10 && c <= 20) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    // Задание 2
+    public static void number(int a) {
+        if (a >= 0) {
+            System.out.println("Число положительное");
+        } else {
+            System.out.println("Число отрицательное");
+        }
+    }
+
+    // Задание 3
+    public static boolean numberBoolean(int a) {
+        if (a < 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    // Задание 4
+    public static boolean cycle(int a, String message) {
+        do {
+            System.out.println(message);
+            a--;
+        } while (a > 0);
+        return false;
+    }
+
+    //Задание 5 (вискокосный год)
+    public static boolean years (int year) {
+        if (year % 400 == 0) {
+            return true;
+        }
+        else if (year % 4 == 0 && year % 100 != 0) {
+            return true;
+        }
+        else {
+            return false;
         }
     }
 
