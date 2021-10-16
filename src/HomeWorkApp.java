cf699229ca164b0ba63bc7484bf56c094f71936f
DanyRDS <dborodin3@gmail.com>
2021-10-16 21:55:40 +0300

    Оптимизация второго ДЗ. (задание 5)

--- src/HomeWorkApp.java
+++ src/HomeWorkApp.java
@@ -116,11 +116,6 @@ public class HomeWorkApp {
         if (year % 400 == 0) {
             return true;
         }
-        else if (year % 4 == 0 && year % 100 != 0) {
-            return true;
-        }
-        else {
-            return false;
-        }
+        else return year % 4 == 0 && year % 100 != 0;
     }
 }
